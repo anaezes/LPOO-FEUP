b1d0bcbe346d207af13a4efcfa91d0c5fad02a57
anaezes <anaezes@gmail.com>
2017-02-17 22:12:44 +0000

    Task1 complete

--- DungeonKeep/src/DungeonKeep.java
+++ DungeonKeep/src/DungeonKeep.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class DungeonKeep {
@@ -26,21 +27,23 @@ public class DungeonKeep {
 		printBoard();
 
 		Scanner input = new Scanner(System.in);
-		String keyCode = input.nextLine();		
-		input.close();		
-		System.out.println(keyCode);
-		
-		keyPressed(keyCode);
-		
-		printBoard();
 
+		while(input.hasNext())
+		{
+			String keyCode = input.nextLine();			
+			System.out.println(keyCode);
+			keyPressed(keyCode);
+			printBoard();
+		}
+		
+		input.close();	
 	}
 
 
 	public void printBoard()
 	{
 		System.out.println();
-		
+
 		for(int i = 0; i < numberRows; i++)
 		{
 			for(int j = 0; j < numberRows; j++)
@@ -58,33 +61,36 @@ public class DungeonKeep {
 
 
 
-	public void keyPressed(String keyCode) {
+	public void keyPressed(String keyCode){
 		char option = keyCode.charAt(0);
-
-		switch( option) { 
+		
+		switch(option) { 
 		//left
 		case 'a':
-			System.out.println(board[hero[0]-1][hero[1]]);
-			if(board[hero[0]-1][hero[1]] == ' ')
+			if(board[hero[1]][hero[0]-1] == ' '){
 				hero[0]--;
-			
+			}		
 			break;
 			//right
 		case 'd':
-			System.out.println(board[hero[0]+1][hero[1]]);
-			if(board[hero[0]+1][hero[1]] == ' ')
+			if(board[hero[1]][hero[0]+1] == ' '){
 				hero[0]++;
+			}
 			break;
 			//up
 		case 'w':
-
+			if(board[hero[1]-1][hero[0]] == ' '){
+				hero[1]--;
+			}
 			break;
 			//down
 		case 's' :
-
+			if(board[hero[1]+1][hero[0]] == ' '){
+				hero[1]++;
+			}
 			break;
 		}
-	} 
+	}
 
 
 

4a223e1cff1e51bcced01f11448c57f29ef120c2
anaezes <anaezes@gmail.com>
2017-02-17 22:42:50 +0000

    Task2 complete

--- DungeonKeep/src/DungeonKeep.java
+++ DungeonKeep/src/DungeonKeep.java
@@ -1,11 +1,10 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class DungeonKeep {
-	static int numberRows = 10;
-	static int[] hero = {1,1};
-	static int[] guard = {8,1};
-	static char[][] board = {{'X','X','X','X','X','X','X','X','X','X'},
+	int numberRows = 10;
+	int[] hero = {1,1};
+	int[] guard = {8,1};
+	char[][] board = {{'X','X','X','X','X','X','X','X','X','X'},
 			{'X',' ',' ',' ','I', ' ', 'X', ' ', ' ', 'X'},
 			{'X','X','X',' ','X', 'X', 'X', ' ', ' ', 'X'},
 			{'X',' ','I',' ','I', ' ', 'X', ' ', ' ', 'X'},
@@ -15,6 +14,9 @@ public class DungeonKeep {
 			{'X','X','X',' ','X', 'X', 'X', 'X', ' ', 'X'},
 			{'X',' ','I',' ','I', ' ', 'X', 'K', ' ', 'X'},
 			{'X','X','X','X','X','X','X','X','X','X'}};
+	boolean onLever = false;
+	boolean winGame = false;
+
 
 	public static void main(String[] args)
 	{
@@ -28,14 +30,18 @@ public class DungeonKeep {
 
 		Scanner input = new Scanner(System.in);
 
-		while(input.hasNext())
+		while(!winGame && input.hasNext())
 		{
+			
 			String keyCode = input.nextLine();			
 			System.out.println(keyCode);
 			keyPressed(keyCode);
 			printBoard();
+			
+			if(winGame)
+				System.out.print("YOU WIN!");			
 		}
-		
+
 		input.close();	
 	}
 
@@ -63,35 +69,60 @@ public class DungeonKeep {
 
 	public void keyPressed(String keyCode){
 		char option = keyCode.charAt(0);
-		
+
 		switch(option) { 
 		//left
 		case 'a':
-			if(board[hero[1]][hero[0]-1] == ' '){
+			if(board[hero[1]][hero[0]-1] == ' ')
+			{
 				hero[0]--;
-			}		
+				onLever = false;
+			}
+			else if(board[hero[1]][hero[0]-1] == 'K')
+			{
+				hero[0]--;
+				activateLever();
+				onLever = true;
+			}
+			else if(board[hero[1]][hero[0]-1] == 'S')
+			{
+				hero[0]--;
+				activateLever();
+				winGame = true;
+			}
 			break;
 			//right
 		case 'd':
-			if(board[hero[1]][hero[0]+1] == ' '){
+			if(board[hero[1]][hero[0]+1] == ' ')
+			{
 				hero[0]++;
+				onLever = false;
 			}
 			break;
 			//up
 		case 'w':
-			if(board[hero[1]-1][hero[0]] == ' '){
+			if(board[hero[1]-1][hero[0]] == ' ')
+			{
 				hero[1]--;
+				onLever = false;
 			}
 			break;
 			//down
 		case 's' :
-			if(board[hero[1]+1][hero[0]] == ' '){
+			if(board[hero[1]+1][hero[0]] == ' ')
+			{
 				hero[1]++;
+				onLever = false;
 			}
 			break;
 		}
+		
+		
 	}
 
-
+	public void activateLever() {
+		board[5][0] = 'S';
+		board[6][0] = 'S';
+	}
 
 }

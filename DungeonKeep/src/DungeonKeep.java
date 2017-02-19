135ad687744a42c349431bf8f75a1022032035b9
cristiana <ribe.cristiana@gmail.com>
2017-02-19 18:58:53 +0000

    task 5 done

--- DungeonKeep/src/DungeonKeep.java
+++ DungeonKeep/src/DungeonKeep.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class DungeonKeep {
 	int numberRows = 10;
@@ -20,6 +21,7 @@ public class DungeonKeep {
 
 	//int[] heroTwo= {1,7};
 	int[] crazyOgre= {4,1};
+	int[] club={4,2};
 	char[][] boardTwo = {{'X','X','X','X','X','X','X','X','X'},
 			{'I',' ',' ',' ',' ', ' ', ' ', 'k', 'X'},
 			{'X',' ',' ',' ',' ', ' ', ' ', ' ', 'X'},
@@ -34,6 +36,7 @@ public class DungeonKeep {
 	boolean winGame = false;
 	boolean lostGame = false;
 	boolean onLeverOgre = false;
+	boolean onLeverClub = false;
 	Level level;
 
 	public enum Level{
@@ -49,8 +52,7 @@ public class DungeonKeep {
 
 	public void run()
 	{
-		level = Level.LEVELONE;
-
+		level = Level.LEVELONE;	
 		printBoard();
 
 		Scanner input = new Scanner(System.in);
@@ -115,6 +117,14 @@ public class DungeonKeep {
 						else
 							System.out.print('0');
 					}
+					else if (club[0]==j && club[1]==i)
+					{
+						if (onLeverClub)
+							System.out.print('$');
+						else 
+							System.out.print('*');
+
+					}
 					else
 						System.out.print(boardTwo[i][j]);
 					
@@ -138,6 +148,7 @@ public class DungeonKeep {
 		else
 		{
 			movesCrazyOgre();
+			//moveOgreClub();
 			board = boardTwo;
 		}
 
@@ -172,7 +183,6 @@ public class DungeonKeep {
 			}
 
 			break;
-			//right
 		case 'd':
 			if(board[hero[1]][hero[0]+1] == ' ')
 				hero[0]++;
@@ -245,6 +255,10 @@ public class DungeonKeep {
 			if(((crazyOgre[0] == (hero[0]+1) || crazyOgre[0] == (hero[0]-1)) && crazyOgre[1] == (hero[1])) || 
 					(crazyOgre[1] == (hero[1]+1) || crazyOgre[1] == (hero[1]-1)) && crazyOgre[0] == (hero[0]))
 				lostGame = true;
+			
+			if(((club[0] == (hero[0]+1) || club[0] == (hero[0]-1)) && club[1] == (hero[1])) || 
+					(club[1] == (hero[1]+1) || club[1] == (hero[1]-1)) && club[0] == (hero[0]))
+				lostGame = true;
 		}
 	}
 
@@ -256,31 +270,152 @@ public class DungeonKeep {
 
 		onLeverOgre = false;
 		
+		
 		switch(num) { 
 		//left
 		case 0:
-			if(boardTwo[crazyOgre[1]][crazyOgre[0]-1] == ' ')
+			if(boardTwo[crazyOgre[1]][crazyOgre[0]-1] != 'X' && boardTwo[crazyOgre[1]][crazyOgre[0]-1] != 'I' && boardTwo[crazyOgre[1]][crazyOgre[0]-1] != 'S' )
 				crazyOgre[0]--;
+				//club[0]--;
+				checkAsterisk();
 			
 			break;
 			//right
 		case 1:
-			if(boardTwo[crazyOgre[1]][crazyOgre[0]+1] == ' ' || boardTwo[crazyOgre[1]][crazyOgre[0]+1] == 'k' || boardTwo[crazyOgre[1]][crazyOgre[0]+1] == 'K')
+			if(boardTwo[crazyOgre[1]][crazyOgre[0]+1] != 'X' && boardTwo[crazyOgre[1]][crazyOgre[0]+1] != 'I' && boardTwo[crazyOgre[1]][crazyOgre[0]+1] != 'S'  ) //|| boardTwo[crazyOgre[1]][crazyOgre[0]+1] == 'k' || boardTwo[crazyOgre[1]][crazyOgre[0]+1] == 'K')
 				crazyOgre[0]++;
+				//club[0]++;
+				checkAsterisk();
+
 			break;
 			//up
 		case 2:
-			if(boardTwo[crazyOgre[1]-1][crazyOgre[0]] == ' ' || boardTwo[crazyOgre[1]-1][crazyOgre[0]] == 'k' || boardTwo[crazyOgre[1]-1][crazyOgre[0]] == 'K')
+			if(boardTwo[crazyOgre[1]-1][crazyOgre[0]] != 'X' && boardTwo[crazyOgre[1]-1][crazyOgre[0]] != 'I' && boardTwo[crazyOgre[1]-1][crazyOgre[0]] != 'S'  )//|| boardTwo[crazyOgre[1]-1][crazyOgre[0]] == 'k' || boardTwo[crazyOgre[1]-1][crazyOgre[0]] == 'K')
 				crazyOgre[1]--;
+				//club[1]--;
+				checkAsterisk();
+
 			break;
 			//down
 		case 3:
-			if(boardTwo[crazyOgre[1]+1][crazyOgre[0]] == ' ')
+			if(boardTwo[crazyOgre[1]+1][crazyOgre[0]] != 'X' && boardTwo[crazyOgre[1]+1][crazyOgre[0]] != 'I' && boardTwo[crazyOgre[1]+1][crazyOgre[0]] != 'S')
 				crazyOgre[1]++;
+				//club[1]++;
+				checkAsterisk();
+
 			break;
 		}
 			
 		if(boardTwo[crazyOgre[1]][crazyOgre[0]] == 'k' || boardTwo[crazyOgre[1]][crazyOgre[0]] == 'K')
 			onLeverOgre = true;
 	}
-}
+	
+	public void moveOgreClub(int[] array)
+	{ 
+		onLeverClub = false;
+		
+		Random club_dir= new Random();
+		
+		int pos = club_dir.nextInt(array.length);
+		int num = array[pos];
+		System.out.println(num);
+		
+		System.out.println(crazyOgre[0]);
+		System.out.println(crazyOgre[1]);
+		System.out.println("club");
+		System.out.println(club[0]);
+		System.out.println(club[1]);
+
+		
+		switch(num) { 
+		//left
+		case 0:
+			if(boardTwo[crazyOgre[1]][crazyOgre[0]-1] != 'X' && boardTwo[crazyOgre[1]][crazyOgre[0]-1] != 'I' && boardTwo[crazyOgre[1]][crazyOgre[0]-1] != 'S')
+			{
+				
+				club[0] = crazyOgre[0] - 1;
+				club[1] = crazyOgre[1];
+			}
+			
+			break;
+			//right
+		case 1:
+			if(boardTwo[crazyOgre[1]][crazyOgre[0] + 1] != 'X' && boardTwo[crazyOgre[1]][crazyOgre[0] + 1] != 'I' && boardTwo[crazyOgre[1]][crazyOgre[0] + 1] != 'S')
+			{
+				club[0] = crazyOgre[0] + 1;
+				club[1] = crazyOgre[1];
+			}
+			break;
+			//up
+		case 2:
+			if(boardTwo[crazyOgre[1]-1][crazyOgre[0]] != 'X' && boardTwo[crazyOgre[1]-1][crazyOgre[0]] != 'I' && boardTwo[crazyOgre[1]-1][crazyOgre[0]] != 'S')
+			{
+				club[1] = crazyOgre[1] - 1;
+				club[0] = crazyOgre[0];
+			}
+			break;
+			//down
+		case 3:
+			if(boardTwo[crazyOgre[1]+1][crazyOgre[0]] != 'X' && boardTwo[crazyOgre[1]+1][crazyOgre[0]] != 'I' && boardTwo[crazyOgre[1]+1][crazyOgre[0]] != 'S')
+			{
+				club[1] = crazyOgre[1] + 1;
+				club[0] = crazyOgre[0];
+			}
+			break;
+		}
+		
+		if(boardTwo[club[1]][club[0]] == 'k' || boardTwo[club[1]][club[0]] == 'K')
+			onLeverClub = true;
+	}
+	
+	public void checkAsterisk(){
+		
+		if(crazyOgre[1]==1){
+			if (crazyOgre[0]==1){
+				int[] array = {1,3};
+				moveOgreClub(array);
+			}
+			
+			if(crazyOgre[0] ==8){
+				int[] array = {0,3};
+				moveOgreClub(array);
+			}
+			else{
+				int[] array = {0,1,3};
+				moveOgreClub(array);
+			}
+		}
+		
+		if (crazyOgre[1]==8){
+			if (crazyOgre[1]==1){
+				int[] array = {1,2};
+				moveOgreClub(array);
+			}
+			
+			if(crazyOgre[1]==8){
+				int[] array = {0,2};
+				moveOgreClub(array);
+			}
+			else{
+				int[] array = {0,1,2};
+				moveOgreClub(array);
+			}
+		}
+		
+		if(crazyOgre[0] == 1) {
+			int[] array = {1, 2, 3};
+			moveOgreClub(array);
+		}
+		
+		if(crazyOgre[0] == 8) {
+			int[] array = {0, 2, 3};
+			moveOgreClub(array);
+		}
+		
+		int[] array = {0, 1, 2, 3 };
+		moveOgreClub(array);
+	}
+}	
+
+
+  

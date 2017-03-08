162e651d69c4de3131ed6258d4dee913554ec027
cristiana <ribe.cristiana@gmail.com>
2017-03-08 22:27:18 +0000

    create test for task3

--- DungeonKeep/src/dkeep/test/TestDungeonGameLogic.java
+++ DungeonKeep/src/dkeep/test/TestDungeonGameLogic.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import dkeep.logic.*;
+import dkeep.logic.Character;
 import dkeep.logic.Game.EnumGameState;
+import dkeep.logic.Game.EnumLevel;
 import ckeep.cli.InputUser.EnumMoves;
 
 public class TestDungeonGameLogic {
@@ -13,6 +15,12 @@ public class TestDungeonGameLogic {
 			{'X', 'H', ' ', 'G', 'X'},
 			{'S', ' ', ' ', ' ', 'X'},
 			{'S', 'k', ' ', ' ', 'X'},
+			{'X', 'X', 'X', 'X', 'X'}};
+	
+	char[][] mapWithOgre = {{'X', 'X', 'X', 'X', 'X'},
+			{'X', 'H', ' ', ' ', 'X'},
+			{'S', ' ', 'O', ' ', 'X'},
+			{'S', 'k', '*', ' ', 'X'},
 			{'X', 'X', 'X', 'X', 'X'}};
 
 	@Test
@@ -67,6 +75,54 @@ public class TestDungeonGameLogic {
 		assertTrue(game.isActiveLever());
 		assertFalse(game.isGameOver());
 		assertEquals(EnumGameState.Win, game.getGameState());
+	}
+	
+	@Test
+	public void testLeverObject() {
+		Lever lever = new Lever(true);
+		assertEquals(true, lever.GetLeverState());
+		lever.SetLeverState(false);
+		assertEquals(false, lever.GetLeverState());
+	}
+	
+	@Test(timeout=100)
+	public void testOgreMove(){
+		GameMap gameMap = new GameMap(mapWithOgre);
+		Game game = new Game(gameMap);
+		game.setGameLevel(EnumLevel.LEVELTWO);
+		assertEquals(game.getGameLevel(), EnumLevel.LEVELTWO);
+		Ogre ogre = new Ogre(2, 2, 3, 2);
+		boolean up=false, down=false, left=false, right= false;
+		while( up == false && down ==false && left == false && right == false) {
+			String oldCoords = ogre.getCordinates();
+			System.out.println(ogre.getCordinates());
+
+			ogre.move(gameMap);
+			
+			System.out.println(ogre.getCordinates());
+			String newCoords = ogre.getCordinates();
+			
+			assertNotEquals(oldCoords, newCoords);
+
+			if(ogre.getXCoordinate() == 1) {
+				System.out.println("UP");
+				up = true;
+			}
+			else if(ogre.getXCoordinate() == 3) {
+				System.out.println("DONW");
+				down = true;
+			}
+			else if(ogre.getYCoordinate() == 1) {
+				System.out.println("LEFT");
+				left = true;
+			}
+			else if(ogre.getYCoordinate() == 3) {
+				System.out.println("RIGHT");
+				right = true;
+			} else {
+				fail("fail test, ogre dont move to the expected possible place");
+			}
+		}		
 	}
 	
 

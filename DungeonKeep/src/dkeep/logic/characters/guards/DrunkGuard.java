72a45cfb48898feb8aa980df238046dae6554aa5
anaezes <anaezes@gmail.com>
2017-03-23 22:13:26 +0000

    DrunkGuard: code refactored

--- DungeonKeep/src/dkeep/logic/characters/guards/DrunkGuard.java
+++ DungeonKeep/src/dkeep/logic/characters/guards/DrunkGuard.java
@@ -16,38 +16,18 @@ public class DrunkGuard extends Guard {
 
 	@Override
 	public void move(GameMap board) {
-
-		char caracter = 'G';
-		int num;
-		Random oj = new Random();
-
-		if(direction == 'f')
-			num = oj.nextInt(2);
-		else if(direction == 'b')
-			num = oj.nextInt(2)+1;
-		else 
-			num = oj.nextInt(3);
-
+		char caracter = 'G';	
+		int num = computeRandomDirection();
 
 		int index = indexGuard;
 		switch(num) {
 		// advance
 		case 0:
-			if(index < getPathSize()-1)
-				index++;
-			else
-				index = 0;
-			direction = 'f';
+			index = moveFront();
 			break;
 			// go back
 		case 2:
-			if(index > 0 && index < getPathSize()-1)
-				index--;
-			else if(index == 0)
-				index = getPathSize()-1;
-			else
-				index = 0;
-			direction = 'b';
+			index = moveBack();
 			break;
 			// sleeps	
 		case 1:
@@ -60,5 +40,42 @@ public class DrunkGuard extends Guard {
 		this.character = caracter;
 		this.SetXCoordinate(x_position[index]);
 		this.SetYCoordinate(y_position[index]);
+	}
+
+	private int computeRandomDirection() {
+		Random oj = new Random();
+		int num;
+		if(direction == 'f')
+			num = oj.nextInt(2);
+		else if(direction == 'b')
+			num = oj.nextInt(2)+1;
+		else 
+			num = oj.nextInt(3);
+		return num;
+	}
+
+	private int moveBack() {
+		int index = indexGuard;
+		
+		if(index > 0 && index < getPathSize()-1)
+			index--;
+		else if(index == 0)
+			index = getPathSize()-1;
+		else
+			index = 0;
+		
+		direction = 'b';
+		return index;
+	}
+
+
+	private int moveFront() {
+		int index = indexGuard;
+		if(index < getPathSize()-1)
+			index++;
+		else
+			index = 0;
+		direction = 'f';
+		return index;
 	}
 }
